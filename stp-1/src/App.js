ce247fff62f3770a35fc6195bf1486f6e5104ddd
LuizKramer <luizeduardockramer@gmail.com>
2023-03-27 11:29:56 -0300

    Create Component

--- stp-1/src/App.js
+++ stp-1/src/App.js
@@ -2,9 +2,32 @@
 import './App.css';
 
 function App() {
+  const name = "Luiz Kramer"
   return (
-    <div className="App">Hello World</div>
+    <div className="App">
+      <Component nome="Luiz Kramer" email="luizeckramer@gmail.com" idade= {22}/>
+      <Component2></Component2>
+    </div>
   );
+}
+
+const Component = (props) => {
+  return (
+    <div>
+      <h1>{props.nome}</h1>
+      <h3>{props.email} <br></br>
+      idade: {props.idade}</h3>
+      
+    </div>
+  );
+}
+
+const Component2 = () =>{
+  return (
+    <div>
+      <p>This is a paragraph</p>
+    </div>
+  )
 }
 
 export default App;
