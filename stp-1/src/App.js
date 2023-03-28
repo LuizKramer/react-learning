5d045ee463eabce820ddd850772ad1130c9f1991
LuizKramer <luizeduardockramer@gmail.com>
2023-03-28 13:38:52 -0300

    something

--- stp-1/src/App.js
+++ stp-1/src/App.js
@@ -1,33 +1,54 @@
 
 import './App.css';
-
 function App() {
-  const name = "Luiz Kramer"
+  const planets = [
+    {name: "Mars", isGasPlanet: false},
+    {name: "Earth", isGasPlanet: false},
+    {name: "Jupiter", isGasPlanet: true},
+    {name: "Venus", isGasPlanet: false},
+    {name: "Neptune", isGasPlanet: true},
+    {name: "Uranus", isGasPlanet: true},
+  ]
   return (
     <div className="App">
-      <Component nome="Luiz Kramer" email="luizeckramer@gmail.com" idade= {22}/>
-      <Component2></Component2>
+      {
+        planets.map((planet, key) => {
+          return <ObjectComponent key={key} name={planet.name} isGasPlanet={planet.isGasPlanet} />;
+        })
+      }
     </div>
   );
 }
 
-const Component = (props) => {
-  return (
-    <div>
-      <h1>{props.nome}</h1>
-      <h3>{props.email} <br></br>
-      idade: {props.idade}</h3>
+const ObjectComponent = (props) => {
+
+  if(!props.isGasPlanet){
+    return (
+      <div>
+        {props.name} 
+      </div>
+    )
+  }
+}
+
+
+// const Component = (props) => {
+//   return (
+//     <div>
+//       <h1>{props.nome}</h1>
+//       <h3>{props.email} <br></br>
+//       idade: {props.idade}</h3>
       
-    </div>
-  );
-}
+//     </div>
+//   );
+// }
 
-const Component2 = () =>{
-  return (
-    <div>
-      <p>This is a paragraph</p>
-    </div>
-  )
-}
+// const Component2 = () =>{
+//   return (
+//     <div>
+//       <p>This is a paragraph</p>
+//     </div>
+//   )
+// }
 
 export default App;
